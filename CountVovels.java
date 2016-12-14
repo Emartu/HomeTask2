ead18519a59e830f4b0fc7376fd4df217e19f902
Emartu <emartu@yandex.ru>
2016-12-14 21:23:34 +0300

    CountVovels.java
    Sorts a list of words by vovels number

---
+++ CountVovels.java
@@ -0,0 +1,33 @@
+public class CountVovels {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Minskaaaaaas", "Akapulkooo", "Warsawa"};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j <= arr.length - 1; j++) {
+                if (count(arr[j]) <count(arr[i])) {
+                    String min = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = min;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static int count(String S) {
+        int vowelCount = 0;
+        for (int j = 0; j < S.length(); j++) {
+            switch (S.charAt(j)) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vowelCount++;
+                    break;
+                default:
+            }
+        }
+        return vowelCount;
+    }
+}
