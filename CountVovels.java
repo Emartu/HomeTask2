aa7a1a22847d6387af8868e92ed6b5d985d50adc
Yauheni Artsiukh <emartu@yandex.ru>
2016-12-18 15:51:13 +0300

    Commit in master #6 before rebase
    new output message

--- CountVovels.java
+++ CountVovels.java
@@ -3,6 +3,7 @@ System.out.println("Commited in dev branch 2")*/
 public class CountVovels {
     public static void main(String[] args) {
         String[] arr = new String[]{"Minskaaaaaas", "Akapulkooo", "Warsawa"};
+System.out.println("commited in master #6 before rebase")
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j <= arr.length - 1; j++) {
                 if (count(arr[j]) <count(arr[i])) {
@@ -32,7 +33,11 @@ public class CountVovels {
         }
         return vowelCount;
     }
+<<<<<<< dd32b7db93fd0baf38d6b4d48df9bac9d3a4e5fe
 
+=======
+	 
+>>>>>>> Commit in master #6 before rebase
 }
 //First change
 //Second Local Change
