b5b2d2e2d486bf64642acc1ac6e9041560468605
Yauheni Artsiukh <emartu@yandex.ru>
2016-12-18 10:35:05 +0300

    Output is changed
    An additional string is added to output: "Strings are sorteted by vovels number"
    by date 18.12.2016

--- CountVovels.java
+++ CountVovels.java
@@ -11,7 +11,7 @@ public class CountVovels {
             }
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.println("Strings are sorteted by vovels number" + arr[i]);
         }
     }
     public static int count(String S) {
